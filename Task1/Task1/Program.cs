5ba6d97d01e447206c42526e608722fd376bef30
SabirQ <sabirmg@code.edu.az>
2022-02-10 23:21:09 +0400

    Tasks_added

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            #region 1ci versiya
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //if (N % 7 == 0) { Console.WriteLine(N); }
+            //else { Solv1(N,7); }
+
+
+
+
+            #endregion
+
+            #region 2ci versiya
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //Solv2(N, 7);
+            #endregion
+
+            #region 3cu versiya
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //Solv3(N, 7);
+            #endregion 
+
+
+
+        }
+        public static void Solv1(int num1,int num2)
+
+        {
+            
+            int num3 = num1 % num2;
+            num1 = num1 - num2;
+            Console.WriteLine(num1);
+        }
+        public static void Solv2(int num1,int num2)
+        {
+            if (num1 % num2 == 0)
+            { 
+                Console.WriteLine(num1); 
+            }
+            else 
+            {
+                int num3 = num1 % num2;
+                num1 = num1 - num3;
+                Console.WriteLine(num1);
+            }
+
+            
+            
+        }
+        public static void Solv3(int num1,int num2) 
+        {
+            while (num1%num2!=0)
+            {
+                num1--;
+            }
+
+            Console.WriteLine(num1);
+
+        }
+        
+
+
+    }
+
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1ci versiya
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //int B = Convert.ToInt32(Console.ReadLine());
+            //char O = Convert.ToChar(Console.ReadLine());
+            //Solv(N, B, O);
+            #endregion
+
+            #region 2ci versiya
+
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //int B = Convert.ToInt32(Console.ReadLine());
+            //char O = Convert.ToChar(Console.ReadLine());
+            //if (O == '+' || O == '-' || O == '*' || O == '/')
+            //{
+            //    Solv2(N, B, O);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("please choose one of these operators:'+','-','*','/'");
+            //    O = Convert.ToChar(Console.ReadLine());
+            //    Solv2(N, B, O);
+            //}
+            #endregion
+
+
+        }
+        public static void Solv(int num1, int num2, char opr)
+        {
+            int operation = 0;
+            if (opr == '+') { operation = num1 + num2; Console.WriteLine(operation); }
+            else if (opr == '-') { operation = num1 - num2; Console.WriteLine(operation); }
+            else if (opr == '*') { operation = num1 * num2; Console.WriteLine(operation); }
+            else if (opr == '/') { operation = num1 / num2; Console.WriteLine(operation); }
+            else { Console.WriteLine("please choose another operator"); }
+
+
+        }
+        public static void Solv2(int num1, int num2, char opr)
+        {
+            int operation = 0;
+            if (opr == '+') { operation = num1 + num2; }
+            else if (opr == '-') { operation = num1 - num2; }
+            else if (opr == '*') { operation = num1 * num2; }
+            else { operation = num1 / num2; }
+            Console.WriteLine(operation);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
